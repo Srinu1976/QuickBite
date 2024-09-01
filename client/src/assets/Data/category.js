be42449ce791fc549f8c14fb2d6b1defbb16ea3d
Srinu1976 <116065445+Srinu1976@users.noreply.github.com>
2024-09-01 06:38:45 +0530

    added fles

---
+++ client/src/assets/Data/category.js
@@ -0,0 +1,41 @@
+import category1 from "../images/categoryimg1.png";
+import category2 from "../images/categoryimg2.jpg";
+import category3 from "../images/categoryimg3.jpg";
+import category4 from "../images/categoryimg4.jpg";
+import category5 from "../images/categoryimg5.jpg";
+import category6 from "../images/categoryimg6.jpg";
+
+const categories = [
+  {
+    id: "01",
+    name: "Chicken",
+    photo: category1,
+  },
+  {
+    id: "02",
+    name: "Burgers",
+    photo: category2,
+  },
+  {
+    id: "03",
+    name: "Pizzas",
+    photo: category3,
+  },
+  {
+    id: "04",
+    name: "Kebabs",
+    photo: category4,
+  },
+  {
+    id: "05",
+    name: "Pastas",
+    photo: category5,
+  },
+  {
+    id: "06",
+    name: "Desserts",
+    photo: category6,
+  },
+];
+
+export default categories;
---
+++ client/src/assets/Data/foodData.js
@@ -0,0 +1,151 @@
+import foodImg1 from "../images/food-img1.jpg";
+import foodImg2 from "../images/food-img2.jpg";
+import foodImg3 from "../images/food-img3.jpg";
+import foodImg4 from "../images/food-img4.jpg";
+import foodImg5 from "../images/food-img5.jpg";
+import foodImg6 from "../images/chickenbiryani.png";
+import foodImg7 from "../images/food-img7.jpg";
+import foodImg8 from "../images/food-img8.jpg";
+
+const menus = [
+  {
+    id: "01",
+    name: "Beaf Burger",
+    category: "Burger",
+    price: 320,
+    desc: "This is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+        reviewText: 'Very Nice'
+      },
+    ],
+    avgRating: 4.5,
+    photo: foodImg1,
+    special: true,
+  },
+  {
+    id: "02",
+    name: "Cheese pizza",
+    category: "Pizza",
+    price: 320,
+    desc: "This is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+        reviewText: 'Very Nice'
+      },
+    ],
+    avgRating: 4.5,
+    photo: foodImg2,
+    special: true,
+  },
+  {
+    id: "03",
+    name: "Pepperoni pizza",
+    category: "Pizza",
+    price: 320,
+    desc: "This is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+        reviewText: 'Very Nice'
+      },
+    ],
+    avgRating: 4.5,
+    photo: foodImg3,
+    special: true,
+  },
+
+  {
+    id: "04",
+    name: "Chicken Burger",
+    category: "Burger",
+    price: 320,
+    desc: "This is the description This is the description This is the description  This is the description  ",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+        reviewText: 'Very Nice'
+      },
+    ],
+    avgRating: 4.5,
+    photo: foodImg4,
+    special: true,
+  },
+  {
+    id: "05",
+    name: "Anday Wala Burger",
+    category: "Burger",
+    price: 320,
+    desc: "This is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+        reviewText: 'Very Nice'
+      },
+    ],
+    avgRating: 4.5,
+    photo: foodImg5,
+    special: true,
+  },
+  {
+    id: "06",
+    name: "Sindhi Biryani",
+    category: "Desi",
+    price: 320,
+    desc: "This is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+        reviewText: 'Very Nice'
+      },
+    ],
+    avgRating: 4.5,
+    photo: foodImg6,
+    special: true,
+  },
+  {
+    id: "07",
+    name: "Chicken Biryani",
+    category: "Desi",
+    price: 320,
+    desc: "This is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+        reviewText: 'Very Nice'
+      },
+    ],
+    avgRating: 4.5,
+    photo: foodImg7,
+    special: true,
+  },
+  {
+    id: "08",
+    name: "Beaf Biryani",
+    category: "Desi",
+    price: 320,
+    desc: "This is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+        reviewText: 'Very Nice'
+      },
+    ],
+    avgRating: 4.5,
+    photo: foodImg8,
+    special: true,
+  },
+  
+];
+
+export default menus;
